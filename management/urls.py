0b793606e957ce0fdddb072dca498dd5311c728b
lgb2002 <cavin12345@naver.com>
2021-08-19 17:39:34 +0900

    commu6

--- management/urls.py
+++ management/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 	path('', views.post_list, name='post_list'),
 	path('contact/', views.contact, name='contact'),
 	path('home/', views.post_list, name='post_list2'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 	path('admin/', views.admin, name='admin'),
 	path('portfolio/', views.portfolio, name='portfolio'),
 	path('test/', views.test, name='test'),
